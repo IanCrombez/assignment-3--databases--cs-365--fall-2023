ffafdaac7c160d1a2fb9eb51b38db6960d8b5097
IanCrombez <icrombez@crombezclan.com>
2023-12-10 17:12:42 -0500

    did the rest of the helpers i hope

--- includes/helpers.php
+++ includes/helpers.php
@@ -84,7 +84,129 @@ function search($search)
     //this is here to tell me if it broke and becuase a try needs a catch
     catch (PDOException $e) 
     {
-        echo 'the search funtion broke',
+        echo '<p>the search funtion broke </p>' . '\n';
         exit;
     }
 }
+
+
+
+
+function update($table, $current_field, $new_value, $find_field, $pattern) 
+{
+    try 
+    {
+        //same as the rest
+        $db = new PDO(
+            "mysql:host=" . DBHOST . ";dbname=" . DBNAME . ";charset=utf8",
+            DBUSER, DBPASS
+        );
+        
+        $encryptionKeyFind = "SET @Key = unhex('" . DBKEY . "')";
+        $db->exec($encryptionKeyFind);
+        //update password based on passord
+        if ($current_field === 'password') 
+        {
+            $update_query = "UPDATE {$table} SET {$current_field} = AES_ENCRYPT(:new_value, @Key) WHERE {$find_field} = :pattern";
+        }
+        //update current field based on passsowrd 
+        else if ($find_field === 'password') 
+        {
+            $update_query = "UPDATE {$table} SET {$current_field} = :new_value WHERE {$find_field} = AES_ENCRYPT(:pattern, @Key)";
+        }
+        //upadte other based on other
+        else 
+        {
+            $update_query = "UPDATE {$table} SET {$current_field} = :new_value WHERE {$find_field} = :pattern";
+        }
+        
+        $statement = $db->prepare($update_query);
+        $statement->bindParam(':new_value', $new_value);
+
+        if ($current_field === 'password' || $find_field === 'password') 
+        {
+            $statement->bindParam(':pattern', $pattern, PDO::PARAM_STR);
+        }
+        else
+        {
+            $statement->bindParam(':pattern', $pattern);
+        }
+
+        $statement->execute();
+    }
+    catch (PDOException $e) 
+    {
+        echo '<p>The update function dint work. </p>' . "\n";
+        exit;
+    }
+}
+
+
+
+
+function insert($site_name, $site_url, $username, $password, $email, $comment) 
+{
+    try 
+    {
+        //same as the rest
+        $db = new PDO(
+            "mysql:host=" . DBHOST . ";dbname=" . DBNAME . ";charset=utf8",
+            DBUSER, DBPASS
+        );
+        $encryptionKeyFind = "SET @Key = unhex('" . DBKEY . "')";
+        $db->exec($encryptionKeyFind);
+        //make the request to update the database
+        $encryptedPassword = "AES_ENCRYPT(:password, @Key)";
+        $insert_query = "
+            INSERT INTO sites (site_name, site_url)
+            VALUES (:site_name, :site_url);
+            INSERT INTO accounts (site_id, username, password, email, comment)
+            SELECT LAST_INSERT_ID(), :username, {$encryptedPassword}, :email, :comment;
+        ";
+        //do all the updating
+        $statement = $db->prepare($insert_query);
+        $statement->bindParam(':site_name', $site_name);
+        $statement->bindParam(':site_url', $site_url);
+        $statement->bindParam(':username', $username);
+        $statement->bindParam(':password',$password);
+        $statement->bindParam(':email', $email);
+        $statement->bindParam(':comment', $comment);
+        $statement->execute();
+
+    } 
+    //if something fails
+    catch (PDOException $e) 
+    {
+        echo '<p>The insert function didnt work. </p>' . "\n";
+        exit;
+    }
+}
+
+
+
+function delete($siteName, $username) {
+    try 
+    {
+        //same as the rest
+        $db = new PDO(
+            "mysql:host=" . DBHOST . ";dbname=" . DBNAME . ";charset=utf8",
+            DBUSER, DBPASS
+        );
+        
+        $encryptionKeyFind = "SET @Key = unhex('" . DBKEY . "')";
+        $db->exec($encryptionKeyFind);
+
+        $deleteAccountsQuery = "DELETE FROM accounts WHERE site_id IN (SELECT site_id FROM sites WHERE site_name = :siteName) And username IN (SELECT username WHERE username = :username)";
+        $statementAccounts = $db->prepare($deleteAccountsQuery);
+        $statementAccounts->bindParam(':siteName', $siteName, PDO::PARAM_STR);
+        $statementAccounts->bindParam(':username', $username, PDO::PARAM_STR);
+        $statementAccounts->execute();
+    } 
+    catch (PDOException $e) 
+    {
+        echo '<p>The delete function dint work. </p>' . "\n";
+        exit;
+    }
+}
+
+
